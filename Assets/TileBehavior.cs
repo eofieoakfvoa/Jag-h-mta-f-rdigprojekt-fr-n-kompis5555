606ab94f3c8a3972c057faf14e17fa92ef672c4c
akeaodkbabkoaeh <114763852+yeaswe@users.noreply.github.com>
2023-11-07 16:29:08 +0100

    GAENUHGEJGAEJG!!!

---
+++ Assets/TileBehavior.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileBehavior
+{
+    Vector3 Position;
+    bool Ground;
+    Tile tile;
+
+}
+public class Grass : TileBehavior
+{
+
+
+}
--- Assets/worldGeneration.cs
+++ Assets/worldGeneration.cs
@@ -1,12 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class worldGeneration : MonoBehaviour
 {
-    
-    public void GenerateWorld(){}
-    public void PerlinNoise(){}
-    public void replaceTile(){}
+    [SerializeField]
+    Tilemap tilemap;
+    [SerializeField]
+    Tile temptile;
+    [SerializeField]
+    Tile tempwaterTile;
+
+    Dictionary<Vector3, Tile> tileDictionary = new();
+    void Start()
+    {
+        GenerateWorld();
+    }  
+    public void GenerateWorld()
+    {
+        for (int x = 0; x < 100; x++)
+        {
+            for (int y = 0; y < 100; y++)
+            {
+                Vector3Int position = new(x,y,0);
+                Tile getTile = PerlinNoise(x,y);
+                tilemap.SetTile(position, getTile);
+            }
+        }
+    }
+    public Tile PerlinNoise(int x, int y)
+    {
+        float Noise = Mathf.PerlinNoise(x, y);
+        print(Noise);
+        if (Noise > 0.5)
+        {
+            return tempwaterTile;
+        }
+        return temptile;
+    }
+    public void ReplaceTile()
+    {
+
+    }
 
 }

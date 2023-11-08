bb66d361ff2b93f5a36f86638de7d69a9f539dc4
akeaodkbabkoaeh <114763852+yeaswe@users.noreply.github.com>
2023-11-08 19:20:07 +0100

    seeds + noise, men behöver göra chunk istället

--- Assets/worldGeneration.cs
+++ Assets/worldGeneration.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Collections;
+using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Tilemaps;
@@ -13,29 +14,73 @@ public class worldGeneration : MonoBehaviour
     Tile temptile;
     [SerializeField]
     Tile tempwaterTile;
+    float seed;
+
+    [SerializeField]
+    int Amplitude;
+
+    [SerializeField]
+    int Frequency;
+    [SerializeField]
+    int Amplitudechangr;
+
+    [SerializeField]
+    int Frequencychangr ;
+    [SerializeField]
+    int Octave;
+    [SerializeField]
+    int MaxX;
+    [SerializeField]
+    int MaxY;
+    [SerializeField]
+    float floatCap;
 
     Dictionary<Vector3, Tile> tileDictionary = new();
     void Start()
     {
+        GetSeed();
         GenerateWorld();
+        
     }  
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GenerateWorld();
+        }
+    }
+    public void GetSeed()
+    {
+        seed = UnityEngine.Random.Range(0, 100);
+    }
     public void GenerateWorld()
     {
-        for (int x = 0; x < 100; x++)
+        for (int x = 0; x < MaxX; x++)
         {
-            for (int y = 0; y < 100; y++)
+            for (int y = 0; y < MaxY; y++)
             {
                 Vector3Int position = new(x,y,0);
-                Tile getTile = PerlinNoise(x,y);
+                Tile getTile = PerlinNoise(x,y, Octave, Amplitudechangr, Frequencychangr, Amplitude, Frequency);
                 tilemap.SetTile(position, getTile);
             }
         }
     }
-    public Tile PerlinNoise(int x, int y)
+    public Tile PerlinNoise(int x, int y, int Octaves, int amplitudeChange, int frequencyChange, int amplitude = 1, int frequency = 1)
     {
-        float Noise = Mathf.PerlinNoise(x, y);
+        print(seed);        
+        float maxvalue = 0;
+        float Noise = 0;
+        for (int i = 0; i < Octaves; i++)
+        {
+            Noise += Mathf.PerlinNoise((x/seed) * frequency, (y/seed) * frequency) * amplitude;
+            maxvalue += amplitude;
+            amplitude *= amplitudeChange;
+            frequency *= frequencyChange;
+            
+        }
+        Noise /= maxvalue;
         print(Noise);
-        if (Noise > 0.5)
+        if (Noise > floatCap)
         {
             return tempwaterTile;
         }

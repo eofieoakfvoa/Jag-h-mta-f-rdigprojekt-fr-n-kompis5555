d53f8f7dfcd52494812962094305798f76d390f4
akeaodkbabkoaeh <114763852+yeaswe@users.noreply.github.com>
2023-11-14 16:27:28 +0100

    AF

--- Assets/worldGeneration.cs
+++ Assets/worldGeneration.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using Unity.Collections;
 using Unity.Mathematics;
 using Unity.VisualScripting;
@@ -25,7 +26,7 @@ public class worldGeneration : MonoBehaviour
     int Amplitudechangr;
 
     [SerializeField]
-    int Frequencychangr ;
+    int Frequencychangr;
     [SerializeField]
     int Octave;
     [SerializeField]
@@ -34,8 +35,14 @@ public class worldGeneration : MonoBehaviour
     int MaxY;
     [SerializeField]
     float floatCap;
-
-    Dictionary<Vector3, Tile> tileDictionary = new();
+    [SerializeField]
+    GameObject Player;
+    [SerializeField]
+    Grid mapgrid;
+    
+    int chunksize = 32;
+    Dictionary<System.Numerics.Vector3, Tile> tileDictionary = new();
+    
     void Start()
     {
         GetSeed();
@@ -53,36 +60,76 @@ public class worldGeneration : MonoBehaviour
     {
         seed = UnityEngine.Random.Range(0, 100);
     }
-    public void GenerateWorld()
+    public void GenerateChunks(UnityEngine.Vector3 pos)
     {
-        for (int x = 0; x < MaxX; x++)
+        //kolla ifall tiledictionary kordinaterna är tom eller inte sen skapa
+        GameObject chunkMap = new GameObject("Tilemap");
+        chunkMap.name = pos.x.ToString() + ", "+ pos.y.ToString() + " Chunk";
+        chunkMap.AddComponent<Tilemap>();
+        chunkMap.AddComponent<TilemapRenderer>();
+        chunkMap.transform.parent = mapgrid.transform;
+        GetTileData(chunkMap.GetComponent<Tilemap>(), pos);
+        tileDebug(chunkMap.GetComponent<Tilemap>());
+    }
+    public void GetTileData(Tilemap map, UnityEngine.Vector3 pos)
+    {
+        for (int x = (int)pos.x; x <= pos.x + chunksize; x ++)
         {
-            for (int y = 0; y < MaxY; y++)
+            for (int y = (int)pos.y; y <= pos.y + chunksize; y ++)
             {
-                Vector3Int position = new(x,y,0);
+                Vector3Int position = new((int)pos.x + x, (int)pos.y + y,0);
                 Tile getTile = PerlinNoise(x,y, Octave, Amplitudechangr, Frequencychangr, Amplitude, Frequency);
-                tilemap.SetTile(position, getTile);
+                map.SetTile(position, getTile);
             }
         }
     }
-    public Tile PerlinNoise(int x, int y, int Octaves, int amplitudeChange, int frequencyChange, int amplitude = 1, int frequency = 1)
+    public void GenerateWorld()
     {
-        print(seed);        
+        for (int x = 0; x < MaxX; x += chunksize)
+        {
+            for (int y = 0; y < MaxY; y += chunksize)
+            {
+                Vector3Int position = new(x,y,0);
+                Tile getTile = PerlinNoise(x,y, Octave, Amplitudechangr, Frequencychangr, Amplitude, Frequency);
+                GenerateChunks(position);
+                //tilemap.SetTile(position, getTile);
+            }
+        }
+    }
+    public void tileDebug(Tilemap map)
+    {
+        BoundsInt bounds = map.cellBounds;
+        TileBase[] allTiles = map.GetTilesBlock(bounds);
+
+        for (int x = 0; x < bounds.size.x; x++) {
+            for (int y = 0; y < bounds.size.y; y++) {
+                TileBase tile = allTiles[x + y * bounds.size.x];
+                if (tile != null) {
+                    Debug.Log("x:" + x + " y:" + y + " tile:" + tile.name);
+                } else {
+                    Debug.Log("x:" + x + " y:" + y + " tile: (null)");
+                }
+            }
+        }     
+
+    }
+    public Tile PerlinNoise(int x, int y, int Octaves, int amplitudeChange, int frequencyChange, int amplitude = 1, int frequency = 1)
+    {       
         float maxvalue = 0;
         float Noise = 0;
         for (int i = 0; i < Octaves; i++)
         {
-            Noise += Mathf.PerlinNoise((x/seed) * frequency, (y/seed) * frequency) * amplitude;
+            Noise += Mathf.PerlinNoise(x/seed * frequency, y/seed * frequency) * amplitude;
             maxvalue += amplitude;
             amplitude *= amplitudeChange;
             frequency *= frequencyChange;
             
         }
         Noise /= maxvalue;
-        print(Noise);
         if (Noise > floatCap)
         {
-            return tempwaterTile;
+            //Gör det till air, sen har jag lager under mappen där det är vatten vatten är lite större en camera size sen följer vattnet med<
+            return tempwaterTile;   
         }
         return temptile;
     }
